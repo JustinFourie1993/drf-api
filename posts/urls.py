35a8931ea9b6261007efc5c129ae716b5fe8e4f5
JustinFourie1993 <>
2023-11-08 12:01:56 +0000

    added postlist view tests

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,6 @@ from posts import views
 
 
 urlpatterns = [
-    path('posts/', views.Postlist.as_view())
+    path('posts/', views.Postlist.as_view()),
+    path('posts/<int:pk>/', views.PostDetail.as_view()),
 ]
