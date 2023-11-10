3e7b5e8442b59dcafecc763bffede685aac100f4
JustinFourie1993 <>
2023-11-10 13:55:44 +0000

    added extra data to profiles

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Post
+from posts.models import Post
 from likes.models import Like
 
 
@@ -8,22 +8,18 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    like_id = serializers.ModelField()
-
+    like_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
-        if value.size > 1024 * 1024 * 2:
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.height > 4096:
             raise serializers.ValidationError(
-                'Image size larger than 2MB!'
+                'Image height larger than 4096px!'
             )
         if value.image.width > 4096:
             raise serializers.ValidationError(
-                'Image width larger than 4096px'
-            )
-
-        if value.image.height > 4096:
-            raise serializers.ValidationError(
-                'Image height larger than 4096px'
+                'Image width larger than 4096px!'
             )
         return value
 
@@ -32,12 +28,12 @@ class PostSerializer(serializers.ModelSerializer):
         return request.user == obj.owner
 
     def get_like_id(self, obj):
-        user = self.context['request']
+        user = self.context['request'].user
         if user.is_authenticated:
             like = Like.objects.filter(
                 owner=user, post=obj
             ).first()
-            return like.if if like else NotImplemented
+            return like.id if like else None
         return None
 
     class Meta:
@@ -45,5 +41,6 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'image_filter', 'like_id'
+            'title', 'content', 'image', 'image_filter',
+            'like_id',
         ]
