24498ca0bcb49c4085ee36c85526e92ac3f19e25
JustinFourie1993 <>
2023-11-13 08:45:00 +0000

    0

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -60,8 +60,8 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEV' in os.environ
-DEBUG = True
+DEBUG = 'DEV' in os.environ
+
 
 
 ALLOWED_HOSTS = ['8000-justinfourie1993-drf-api-yk74avgeuw.us2.codeanyapp.com',
